498f794578fdc7979cdcf0b2a313a63558ac7bd1
Rohan <rohan8dave@gmail.com>
2025-02-03 16:48:18 -0500

    change to perp + beacon indexing

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,105 +3,42 @@ import {
 } from '@graphprotocol/graph-ts'
 
 import {
-  EventToken as EventTokenEvent,
-  Transfer   as TransferEvent,
-} from '../generated/Poap/Poap'
+  PositionOpened as PositionOpenedEvent,
+  PositionClosed as PositionClosedEvent,
+} from '../generated/Perp/Perp'
 
 import {
-  Token,
-  Account,
-  Event,
-  Transfer,
+  DataUpdated as DataUpdatedEvent
+} from '../generated/Beacon/Beacon'
+
+import {
+  MarketSnapshot,
+  BeaconSnapshot
 } from '../generated/schema'
 
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+export function handlePositionOpened(event: PositionOpenedEvent): void {
+  let marketSnapshot = new MarketSnapshot(event.transaction.hash);
+  marketSnapshot.market = event.address;
+  marketSnapshot.timestamp = event.block.timestamp;
+  marketSnapshot.markPrice = event.params.newMarkPrice;
 
-
-function createEventID(event: ethereum.Event): string
-{
-  return event.block.number.toString().concat('-').concat(event.logIndex.toString());
+  marketSnapshot.save();
 }
 
-export function handleEventToken(ev: EventTokenEvent): void
-{
-  let event = Event.load(ev.params.eventId.toString());
-  // This handler always run after the transfer handler
-  let token = Token.load(ev.params.tokenId.toString())!;
-  if (event == null) {
-    event               = new Event(ev.params.eventId.toString());
-    event.tokenCount    = BigInt.fromI32(0);
-    event.tokenMints    = BigInt.fromI32(0);
-    event.transferCount = BigInt.fromI32(0);
-    event.created       = ev.block.timestamp
-  }
+export function handlePositionClosed(event: PositionClosedEvent): void {
+  let marketSnapshot = new MarketSnapshot(event.transaction.hash);
+  marketSnapshot.market = event.address;
+  marketSnapshot.timestamp = event.block.timestamp;
+  marketSnapshot.markPrice = event.params.newMarkPrice;
 
-  event.tokenCount    += BigInt.fromI32(1);
-  event.tokenMints    += BigInt.fromI32(1);
-  event.transferCount += BigInt.fromI32(1);
-  token.event         = event.id;
-  token.mintOrder     = event.tokenMints;
-  event.save();
-  token.save();
+  marketSnapshot.save();
 }
 
-export function handleTransfer(ev: TransferEvent): void {
-  let token    = Token.load(ev.params.tokenId.toString());
-  let from     = Account.load(ev.params.from.toHex());
-  let to       = Account.load(ev.params.to.toHex());
-  let transfer = new Transfer(createEventID(ev));
+export function handleDataUpdated(event: DataUpdatedEvent): void {
+  let beaconSnapshot = new BeaconSnapshot(event.transaction.hash);
+  beaconSnapshot.beacon = event.address;
+  beaconSnapshot.timestamp = event.block.timestamp;
+  beaconSnapshot.indexPrice = event.params.data;
 
-  if (from == null) {
-    from              = new Account(ev.params.from.toHex());
-    // The from account at least has to own one token
-    from.tokensOwned  = BigInt.fromI32(1);
-  }
-  // Don't subtracts from the ZERO_ADDRESS (it's the one that mint the token)
-  // Avoid negative values
-  if(from.id != ZERO_ADDRESS) {
-    from.tokensOwned -= BigInt.fromI32(1);
-  }
-  from.save();
-
-  if (to == null) {
-    to              = new Account(ev.params.to.toHex());
-    to.tokensOwned  = BigInt.fromI32(0);
-  }
-  to.tokensOwned += BigInt.fromI32(1);
-  to.save();
-
-  if (token == null) {
-    token               = new Token(ev.params.tokenId.toString());
-    token.transferCount = BigInt.fromI32(0);
-    token.created       = ev.block.timestamp
-  }
-  token.owner = to.id;
-  token.transferCount += BigInt.fromI32(1);
-  token.save();
-
-
-  if (token.event != null) {
-    let event = Event.load(token.event as string);
-
-    if(event != null) {
-      // Add one transfer
-      event.transferCount += BigInt.fromI32(1);
-  
-      // Burning the token
-      if(to.id == ZERO_ADDRESS) {
-        event.tokenCount    -= BigInt.fromI32(1);
-        // Subtract all the transfers from the burned token
-        event.transferCount -= token.transferCount;
-      }
-      event.save();
-    }
-  }
-
-  
-
-  transfer.token       = token.id;
-  transfer.from        = from.id;
-  transfer.to          = to.id;
-  transfer.transaction = ev.transaction.hash;
-  transfer.timestamp   = ev.block.timestamp;
-  transfer.save();
+  beaconSnapshot.save();
 }
